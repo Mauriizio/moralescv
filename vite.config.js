a400d7313e97b28674c900150dd4cd3ef3ae25b5
Maurizio <caballeromaurizio@gmail.com>
2025-07-03 20:32:43 -0400

    otro commit

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]'
-      }
-    }
-  },
-  server: {
-    fs: {
-      strict: false
-    }
-  }
+  base: '',
+  publicDir: 'public'
 })
